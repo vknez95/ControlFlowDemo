b765492ef44d41c20ea19a3b7f439a213f7e2bae
Veljko Knezevic <veljko.knezevic@gmail.com>
2016-12-30 12:24:45 -0500

    introduced map-reduced pattern

--- Program.cs
+++ Program.cs
@@ -8,37 +8,74 @@ namespace ControlFlowDemo
 {
     class Program
     {
+        static int CalculateControlDigit(long number)
+        {
+            IEnumerable<int> digits = GetDigitsFromLeastSignificant(number);
+            IEnumerable<int> factors = GetMultiplicativeFactors();
+            IEnumerable<int> weightedDigits = AddWeight(digits, factors);
+
+            int sum = Sum(weightedDigits);
+
+            int result = sum % 11;
+            if (result == 10)
+                result = 1;
+
+            return result;
+        }
+
+        private static int Sum(IEnumerable<int> values)
+        {
+            int sum = 0;
+
+            foreach (int value in values)
+            {
+                sum += value;
+            }
+
+            return sum;
+        }
+
+        private static IEnumerable<int> AddWeight(IEnumerable<int> values, IEnumerable<int> factors)
+        {
+            IEnumerator<int> factor = factors.GetEnumerator();
+
+            List<int> weightedDigits = new List<int>();
+
+            foreach (int value in values)
+            {
+                factor.MoveNext();
+                weightedDigits.Add(factor.Current * value);
+            }
+
+            return weightedDigits;
+        }
+
+        private static IEnumerable<int> GetMultiplicativeFactors()
+        {
+            return new int[] { 1, 3, 1, 3, 1, 3, 1, 3, 1, 3, 1, 3, 1, 3, 1, 3, 1, 3 };
+        }
+
+        private static IEnumerable<int> GetDigitsFromLeastSignificant(long number)
+        {
+            List<int> digits = new List<int>();
+
+            do
+            {
+                int digit = (int)(number % 10);
+                digits.Add(digit);
+                number /= 10;
+            }
+            while (number > 0);
+
+            return digits;
+        }
+
         static void Main(string[] args)
         {
             Console.WriteLine(CalculateControlDigit(21342182));
 
             Console.Write("Press any key to exit");
             Console.ReadKey();
-        }
-
-        static int CalculateControlDigit(long number)
-        {
-            int sum = 0;
-            int factor = 1;
-
-            do
-            {
-                int digit = (int)(number % 10);
-
-                sum += factor * digit;
-                factor = 4 - factor;
-
-                number /= 10;
-            }
-            while (number > 0);
-
-            int result = sum % 11;
-            if (result == 10)
-            {
-                result = 1;
-            }
-
-            return result;
         }
     }
 }
